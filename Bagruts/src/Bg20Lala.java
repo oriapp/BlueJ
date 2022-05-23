9bb19984105dcf2615e790a0c6c97c6986f18cb6
R C N <plbwymw@gmail.com>
2022-05-23 09:31:17 +0300

    nananana

--- Bagruts/src/Bg20Lala.java
+++ Bagruts/src/Bg20Lala.java
@@ -28,14 +28,53 @@ public class Bg20Lala {
         return found;
     }
 
+    public static String allExist(Stack<Integer> stk) {
+        Stack<Integer> ns = new Stack<>();
+        String hasNum = "true";
+        int totalNums = 0;
+        while (!stk.isEmpty()) {
+            int popedNum = stk.pop();
+            totalNums++;
+            ns.push(popedNum);
+        }
+        int[] hunNums = new int[totalNums];
+        int[] sinNums = new int[totalNums];
+        int count = 0;
+        while(!ns.isEmpty()) {
+            int theNumber = ns.pop();
+            stk.push(theNumber);
+            hunNums[count] = theNumber / 100;
+            sinNums[count] = theNumber % 10;
+            count++;
+        }
+
+        for (int i = 0; i < hunNums.length; i++) {
+            for (int j = 0; j < sinNums.length; j++) {
+                if (hunNums[i] != sinNums[j] || 0 ==  sinNums[j])
+                    hasNum = "false";
+            }
+        }
+
+        return hasNum;
+    }
+
     public static void main(String[] args) {
+//        Stack<Integer> x = new Stack<>();
+//        x.push(162);
+//        x.push(251);
+//        x.push(568);
+//        x.push(77);
+//        System.out.print("Original form -> ");
+//        System.out.println(x);
+//        System.out.println(isExist(8 , x));
+
         Stack<Integer> x = new Stack<>();
-        x.push(162);
+        x.push(122);
         x.push(251);
-        x.push(568);
-        x.push(77);
-        System.out.print("Original from -> ");
-        System.out.println(x);
-        System.out.println(isExist(8 , x));
+        x.push(565);
+        x.push(12334);
+        x.push(28);
+        x.push(7);
+        System.out.println(allExist(x));
     }
 }
